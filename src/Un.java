447d5d87f6b547a36f043cb37c136ef8bdc1b906
YanaMan <manaevaiana@gmail.com>
2021-02-12 23:30:12 +0300

    Initial commit

---
+++ src/Un.java
@@ -0,0 +1,11 @@
+public class Un {
+        public static void main(String[] args) {
+            int g = 2+6;
+            System.out.println("2+6" + " = " + g);
+            int original_g = g;
+
+            int G = g-1;
+            System.out.println(original_g + "-1" + " = " + G);
+        }
+    }
+
